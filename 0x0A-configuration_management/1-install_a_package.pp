157152c39811643088d73cf59e490c2b0df90789
mire-web <amahe8664@gmail.com>
2024-02-24 13:59:52 +0100

    install-flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
 # Install flask 2.1.0 from pip3
-package{ 'flask':
+package{ 'python3-pip':
+  ensure => present,
+}
+
+package{ 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package{ 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['Werkzeug'],
 }
