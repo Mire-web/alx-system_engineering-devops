6bc961fa7927b9a4e788657e1ecc9cb087c7c218
mire-web <amahe8664@gmail.com>
2024-02-26 09:55:55 +0100

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -11,17 +11,6 @@ file_line { 'SSH Private Key':
   append_on_no_match => true
 }
 
-# Regex match explanation
-#
-# ^       beginning of the line
-# [#]*  atleast one hash character
-# [\s]*  zero or more white space characters
-# (?i)IdentityFile case insensitive "IdentityFile"
-# [\s]+ at least one whitespace character
-# ~/.ssh/id_rsa The ssh private key file path we want to replace
-# $      end of the line
-
-
 file_line { 'Deny Password Auth':
   path               => '/etc/ssh/ssh_config',
   line               => '    PasswordAuthentication no',
@@ -29,13 +18,3 @@ file_line { 'Deny Password Auth':
   replace            => true,
   append_on_no_match => true
 }
-
-# Regex match explanation
-#
-# ^       beginning of the line
-# [#]*  atleast one hash character
-# [\s]*  zero or more white space characters
-# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
-# [\s]+ at least one whitespace character
-# (yes|no) with the value "yes" or the value "no"
-# $      end of the line
