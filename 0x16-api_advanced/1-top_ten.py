3f870be2b0a4b825e4c071ba0b78f95b1485d432
mire-web <amahe8664@gmail.com>
2024-06-19 16:53:38 +0100

    try-again

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,12 +12,12 @@ def top_ten(subreddit):
     """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mirey/1.0'}
-    try:
-        response = requests.get(url, allow_redirects=False, headers=headers)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
         response.raise_for_status()
         result = response.json().get('data').get('children')
         titles = [data.get('data').get('title') for data in result]
         for title in titles:
             print(title)
-    except requests.exceptions.RequestException as req_err:
+    else:
         print(None)
