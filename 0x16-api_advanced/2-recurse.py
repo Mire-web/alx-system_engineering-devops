5044426eff062a46711dea5bdb5cfe48eed3669f
mire-web <amahe8664@gmail.com>
2024-06-22 00:44:15 +0100

    my-solution

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,29 @@
+
 #!/usr/bin/python3
 """
-Using reddit's API
+Script to recursively return list containing the title of all hot articles for a subreddit
 """
 import requests
-after = None
+params = {
+    'limit': 100,
+    'after': None
+}
 
 
 def recurse(subreddit, hot_list=[]):
-    """returning top ten post titles recursively"""
-    global after
-    user_agent = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user_agent,
-                           allow_redirects=False)
-
-    if results.status_code == 200:
-        after_data = results.json().get("data").get("after")
-        if after_data is not None:
-            after = after_data
-            recurse(subreddit, hot_list)
-        all_titles = results.json().get("data").get("children")
-        for title_ in all_titles:
-            hot_list.append(title_.get("data").get("title"))
-        return hot_list
-    else:
-        return (None)
+    """Recursive function to retrieve all reddit hot articles in a subreddit"""
+    headers = {'User-Agent': 'Mirey 1.0 by mirey'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    try:
+        res = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        after = res.json().get('data').get('after')
+        data_set = res.json()['data']['children']
+        for data in data_set:
+            hot_list.append(data['data']['title'])
+        if not after:
+            return hot_list
+        else:
+            params['after'] = after
+            return recurse(subreddit, hot_list)
+    except Exception as e:
+        return None
