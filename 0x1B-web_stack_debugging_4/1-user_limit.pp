3a8ed193ff91008e7189541fa08c4e77842b7f76
mire-web <amahe8664@gmail.com>
2024-06-27 04:06:52 +0100

    limti

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 exec {'Increase-hard-limit':
   command => 'sed -i "/holberton/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/bin/bash',
+  path    => '/usr/local/bin/:/bin/',
 }
 
 exec {'Increase-soft-limit':
   command => 'sed -i "/holberton/s/4/40000/" /etc/security/limits.conf',
-  path    => '/usr/bin/bash',
+  path    => '/usr/local/bin/:/bin/',
 }
