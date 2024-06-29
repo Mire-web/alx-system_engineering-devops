3ba7d9cd97f3ecb52978b0a6060f18f854e9c2a2
mire-web <amahe8664@gmail.com>
2024-06-29 07:59:33 +0100

    path-fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # Script to increase nginx file limits
 exec {'Increase Ulimit nginx':
-  command => 'sed -i "s/ULIMIT=.*/ULIMIT=\"-n 4096\"" /etc/default/nginx',
+  command => 'sed -i "s/ULIMIT=.*/ULIMIT=4096" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
 exec {'restart nginx':
-  command => 'service nginx restart',
+  command => 'nginx restart',
   path    => '/etc/init.d'
 }
